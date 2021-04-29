cc99631352710e4adca530a7460a5777181da577
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-04-29 13:36:10 +0900

    feat: change signiture

---
+++ src/Teams.Notifications/TeamsPostService.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Nogic.Teams.Notifications.Entities;
+
+namespace Nogic.Teams.Notifications
+{
+    /// <summary>
+    /// Implementation of <see cref="ITeamsPostService"/>
+    /// </summary>
+    public class TeamsPostService : ITeamsPostService
+    {
+        private readonly HttpClient _client;
+
+        public TeamsPostService(HttpClient client)
+            => _client = client;
+
+        /// <inheritdoc/>
+        public async ValueTask<HttpResponseMessage> PostMessageAsync(Uri webhookUri, MessageCard message, CancellationToken cancellationToken = default)
+            => await _client.PostAsJsonAsync(webhookUri, message, cancellationToken).ConfigureAwait(false);
+    }
+}

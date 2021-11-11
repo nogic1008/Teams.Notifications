9ca5c8bac40b893e9bbb89a3040736218e8c4d56
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-11-11 11:11:38 +0900

    feat: use .NET 6 new features

--- src/Teams.Notifications.Client/AppBase.cs
+++ src/Teams.Notifications.Client/AppBase.cs
@@ -1,26 +1,23 @@
-using System;
-using System.Threading.Tasks;
 using ConsoleAppFramework;
 using Nogic.Teams.Notifications.Entities;
 using Microsoft.Extensions.Logging;
 
-namespace Nogic.Teams.Notifications.Client
+namespace Nogic.Teams.Notifications.Client;
+
+public class AppBase : ConsoleAppBase
 {
-    public class AppBase : ConsoleAppBase
+    private readonly ITeamsPostService _postService;
+    public AppBase(ITeamsPostService postService)
+        => _postService = postService;
+
+    public async ValueTask PostAsync(string uri, string title, string description)
     {
-        private readonly ITeamsPostService _postService;
-        public AppBase(ITeamsPostService postService)
-            => _postService = postService;
+        var webhookUri = new Uri(uri);
+        var message = new MessageCard(Title: title, Text: description);
 
-        public async ValueTask PostAsync(string uri, string title, string description)
-        {
-            var webhookUri = new Uri(uri);
-            var message = new MessageCard(Title: title, Text: description);
+        var res = await _postService.PostMessageAsync(webhookUri, message, Context.CancellationToken).ConfigureAwait(false);
+        res.EnsureSuccessStatusCode();
 
-            var res = await _postService.PostMessageAsync(webhookUri, message, Context.CancellationToken).ConfigureAwait(false);
-            res.EnsureSuccessStatusCode();
-
-            Context.Logger.LogInformation("Posted successfully to Teams channel.");
-        }
+        Context.Logger.LogInformation("Posted successfully to Teams channel.");
     }
 }
--- src/Teams.Notifications/ITeamsPostService.cs
+++ src/Teams.Notifications/ITeamsPostService.cs
@@ -1,20 +1,15 @@
-using System;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 using Nogic.Teams.Notifications.Entities;
 
-namespace Nogic.Teams.Notifications
+namespace Nogic.Teams.Notifications;
+
+public interface ITeamsPostService
 {
-    public interface ITeamsPostService
-    {
-        /// <summary>
-        /// Post a message to Microsoft Teams Channel.
-        /// </summary>
-        /// <param name="webhookUri">Incoming Webhook URI</param>
-        /// <param name="message">Message card object</param>
-        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-        /// <returns>The task object representing the asynchronous operation.</returns>
-        ValueTask<HttpResponseMessage> PostMessageAsync(Uri webhookUri, MessageCard message, CancellationToken cancellationToken = default);
-    }
+    /// <summary>
+    /// Post a message to Microsoft Teams Channel.
+    /// </summary>
+    /// <param name="webhookUri">Incoming Webhook URI</param>
+    /// <param name="message">Message card object</param>
+    /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    ValueTask<HttpResponseMessage> PostMessageAsync(Uri webhookUri, MessageCard message, CancellationToken cancellationToken = default);
 }
